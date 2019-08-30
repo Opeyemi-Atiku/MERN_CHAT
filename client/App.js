911b8b67ee3d4a6d020e9ff844722364f1f7552f
richard <ajibolarichardson96@gmail.com>
2019-08-30 11:36:35 +0100

    first commit

---
+++ client/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import MainRouter from './MainRouter';
+import {BrowserRouter} from 'react-router-dom';
+import { hot } from 'react-hot-loader';
+
+
+const App = () => (
+  <BrowserRouter>
+      <MainRouter/>
+  </BrowserRouter>
+)
+
+export default hot(module)(App)
